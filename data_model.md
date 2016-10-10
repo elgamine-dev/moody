7d063838721cc5170965c5bb17e923f1a7e397b0
Maxime Pawlak <contact@maximepawlak.fr>
2016-10-10 11:22:06 +0200

    Add first firebase test

--- data_model.md
+++ data_model.md
@@ -12,25 +12,30 @@
 	days : {
 		"2016-10-06" : {
 			"UNIQUE_KEY" : {
-				"timestamp" : "2016-10-06T17:29:45Z
+				"day": ""2016-10-06",
+				"ts" : "2016-10-06T17:29:45Z,
 				"value" : "HAPPY"
 			},
 			UNIQUE_KEY" : {
-				"timestamp" : "2016-10-06T17:27:37Z
+				"day": ""2016-10-06",
+				"ts" : "2016-10-06T17:27:37Z,
 				"value" : "MEDIUM"
 			}
 		},
 		"2016-10-05" : {
 			"UNIQUE_KEY" : {
-				"timestamp" : "2016-10-05T16:12:32Z
+				"day": ""2016-10-05",
+				"ts" : "2016-10-05T16:12:32Z,
 				"value" : "SAD"
 			},
 			"UNIQUE_KEY" : {
-				"timestamp" : "2016-10-05T17:04:10Z
+				"day": ""2016-10-05",
+				"ts" : "2016-10-05T17:04:10Z,
 				"value" : "SAD"
 			},
 			"UNIQUE_KEY" : {
-				"timestamp" : "2016-10-05T17:08:36Z
+				"day": ""2016-10-05",
+				"ts" : "2016-10-05T17:08:36Z,
 				"value" : "HAPPY"
 			}
 		}
--- hardware/main.py
+++ hardware/main.py
@@ -1,16 +1,58 @@
-import RPi.GPIO as GPIO
+# import RPi.GPIO as GPIO
 import time
+from firebase import firebase
+import datetime
 
-pinHappy = 18;
+
+
+# Firebase infos
+FIREBASE_DATABASE_URL = 'https://break-manager-ac99b.firebaseio.com'
+FIREBASE_DATABASE_NODE = '/moody/days/'
+
+# Values infos
+VALUE_HAPPY = "HAPPY"
+VALUE_MEDIUM = "MEDIUM"
+VALUE_SAD = "SAD"
+
+# Setup GPIO
+PIN_HAPPY = 18;
 # pinCasual = 18;
 # pinSad = 18;
 
-GPIO.setmode(GPIO.BCM)
+# GPIO.setmode(GPIO.BCM)
+# GPIO.setup(PIN_HAPPY, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-GPIO.setup(pinHappy, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-while True:
-    state_happy = GPIO.input(pinHappy)
-    if state_happy == False:
-        print('Button Happy Pressed')
-        time.sleep(0.5)
+# Go !
+def main():
+	initFirebase()
+	while True:
+	    # state_happy = GPIO.input(PIN_HAPPY)
+	    # if state_happy == False:
+	    if true:
+			print('Button Happy Pressed')
+
+			value = VALUE_HAPPY
+			day = datetime.date.today()
+			now = datetime.datetime.now()
+			postFirebase(value, day, now)
+
+			time.sleep(0.5)
+
+
+def initFirebase():
+	firebase = firebase.FirebaseApplication(FIREBASE_DATABASE_URL, None)
+
+def postFirebase(value, day, timestamp):
+	new_mood = {
+		"value" : "HAPPY",
+		"day" : str(today),
+		"ts" : str(now)
+	};
+
+	result = firebase.post(FIREBASE_DATABASE_NODE+ str(today), new_mood)
+	print result
+
+
+if __name__ == '__main__':
+	main()
