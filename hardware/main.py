b31b0954f5486e578a98f680d645bbc373ff22f9
Maxime Pawlak <contact@maximepawlak.fr>
2016-10-10 12:32:36 +0200

    Add Pin and programm architecture

--- hardware/main.py
+++ hardware/main.py
@@ -14,12 +14,14 @@ VALUE_MEDIUM = "MEDIUM"
 VALUE_SAD = "SAD"
 
 # Setup GPIO
-PIN_HAPPY = 18;
-# pinCasual = 18;
-# pinSad = 18;
+PIN_HAPPY = 17;
+# PIN_MEDIUM = 27;
+# PIN_SAD = 17;
 
 # GPIO.setmode(GPIO.BCM)
 # GPIO.setup(PIN_HAPPY, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+# GPIO.setup(PIN_MEDIUM, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+# GPIO.setup(PIN_SAD, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 
 # Go !
@@ -27,11 +29,35 @@ def main():
 	initFirebase()
 	while True:
 	    # state_happy = GPIO.input(PIN_HAPPY)
+		# state_medium = GPIO.input(PIN_MEDIUM)
+		# state_sad = GPIO.input(PIN_SAD)
+
 	    # if state_happy == False:
 	    if True:
 			print('Button Happy Pressed')
 
 			value = VALUE_HAPPY
+			day = datetime.date.today()
+			now = datetime.datetime.now()
+			postFirebase(value, day, now)
+
+			time.sleep(0.5)
+
+	    # if state_medium == False:
+	    elif True:
+			print('Button Medium Pressed')
+
+			value = VALUE_MEDIUM
+			day = datetime.date.today()
+			now = datetime.datetime.now()
+			postFirebase(value, day, now)
+
+			time.sleep(0.5)
+		# if state_sad == False:
+	    elif True:
+			print('Button SAD Pressed')
+
+			value = VALUE_SAD
 			day = datetime.date.today()
 			now = datetime.datetime.now()
 			postFirebase(value, day, now)
@@ -53,6 +79,15 @@ def postFirebase(value, day, timestamp):
 	result = firebase.post(FIREBASE_DATABASE_NODE+ str(day), new_mood)
 	print result
 
+def onButtonPressed():
+	print('Button SAD Pressed')
+
+	value = VALUE_SAD
+	day = datetime.date.today()
+	now = datetime.datetime.now()
+	postFirebase(value, day, now)
+
+	time.sleep(0.5)
 
 if __name__ == '__main__':
 	main()
